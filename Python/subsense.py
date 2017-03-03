6c831de36ee1737a26c2c3e4c11b9208c049e001
Jonas Bühlmeyer <jonas.buehlmeyer@robotics-erlangen.de>
2017-03-03 18:18:36 +0100

    changed background-... to gradient-.., cause it is more accurate

--- Python/subsense.py
+++ Python/subsense.py
@@ -10,8 +10,8 @@ N = 16
 nmbr_min = 12
 MAX_hamming_weight = 0
 
-# LBSP-UPDATE BACKGROUND PICTURES ==========================================================
-def update_background_pictures(img, background_pictures, rows, cols):
+# LBSP-UPDATE GRADIENT PICTURES ==========================================================
+def update_gradient_pictures(img, gradient_pictures, rows, cols):
     #    0    x   1   x   2
     #    x    3   4   5   x 
     #    6    7   x   8   9
@@ -19,41 +19,41 @@ def update_background_pictures(img, background_pictures, rows, cols):
     #   13    x  14   x   15
     
     # First row
-    background_pictures[:rows-2,:cols-2,0]  = img[2:rows,2:cols]
-    background_pictures[:rows-2,:cols,1]    = img[2:rows,:cols]
-    background_pictures[:rows-2,2:cols,2]   = img[2:rows,:cols-2]
+    gradient_pictures[:rows-2,:cols-2,0]  = img[2:rows,2:cols]
+    gradient_pictures[:rows-2,:cols,1]    = img[2:rows,:cols]
+    gradient_pictures[:rows-2,2:cols,2]   = img[2:rows,:cols-2]
 
     # Second row
-    background_pictures[:rows-1,:cols-1,3]  = img[1:rows,1:cols]
-    background_pictures[:rows-1,:cols,4]    = img[1:rows,:cols]
-    background_pictures[:rows-1,1:cols,5]   = img[1:rows,:cols-1]
+    gradient_pictures[:rows-1,:cols-1,3]  = img[1:rows,1:cols]
+    gradient_pictures[:rows-1,:cols,4]    = img[1:rows,:cols]
+    gradient_pictures[:rows-1,1:cols,5]   = img[1:rows,:cols-1]
 
     # Third row
-    background_pictures[:rows,:cols-2,6]    = img[:rows,2:cols]
-    background_pictures[:rows,:cols-1,7]    = img[:rows,1:cols]
-    background_pictures[:rows,1:cols,8]     = img[:rows,:cols-1]
-    background_pictures[:rows,2:cols,9]     = img[:rows,:cols-2]
+    gradient_pictures[:rows,:cols-2,6]    = img[:rows,2:cols]
+    gradient_pictures[:rows,:cols-1,7]    = img[:rows,1:cols]
+    gradient_pictures[:rows,1:cols,8]     = img[:rows,:cols-1]
+    gradient_pictures[:rows,2:cols,9]     = img[:rows,:cols-2]
 
     # Fourth row
-    background_pictures[1:rows,:cols-1,10]  = img[:rows-1,1:cols]
-    background_pictures[1:rows,:cols,11]    = img[:rows-1,:cols]
-    background_pictures[1:rows,1:cols,12]   = img[:rows-1,:cols-1]
+    gradient_pictures[1:rows,:cols-1,10]  = img[:rows-1,1:cols]
+    gradient_pictures[1:rows,:cols,11]    = img[:rows-1,:cols]
+    gradient_pictures[1:rows,1:cols,12]   = img[:rows-1,:cols-1]
 
     # Fifth row
-    background_pictures[2:rows,:cols-2,13]  = img[:rows-2,2:cols]
-    background_pictures[2:rows,:cols,14]    = img[:rows-2,:cols]
-    background_pictures[2:rows,2:cols,15]   = img[:rows-2,:cols-2]
+    gradient_pictures[2:rows,:cols-2,13]  = img[:rows-2,2:cols]
+    gradient_pictures[2:rows,:cols,14]    = img[:rows-2,:cols]
+    gradient_pictures[2:rows,2:cols,15]   = img[:rows-2,:cols-2]
 
-# LBSP-UPDATE BACKGROUND PICTURES ==========================================================
+# LBSP-UPDATE GRADIENT PICTURES ==========================================================
 
-# LBSP-BACKGROUND DECISION =================================================================
-def lbsp(img, background_pictures):
+# LBSP-GRADIENT DECISION =================================================================
+def lbsp(img, gradient_pictures):
     global T_r
-    background_decision = background_pictures - img[:,:,np.newaxis]
-    foreback = img*0
+    gradient_decision = gradient_pictures - img[:,:,np.newaxis]
+    gradient = img*0
     T = T_r * img
-    comp = background_decision<T[:,:,np.newaxis]
-    foreback[(comp != False).sum(2) < nmbr_min] = 255
+    comp = gradient_decision<T[:,:,np.newaxis]
+    gradient[(comp != False).sum(2) < nmbr_min] = 255
     
     # T_r update ------ Grenzen noch anpassen, eventuell auch Geschwindigkeit der Aenderung
     hammingweight=comp.sum()*1.0/MAX_hamming_weight
@@ -63,11 +63,11 @@ def lbsp(img, background_pictures):
         if(hammingweight > 0.875):
             T_r = T_r - 0.001
         
-    return foreback
-# LBSP-BACKGROUND DECISION =================================================================
+    return gradient
+# LBSP-GRADIENT DECISION =================================================================
 
 
-cap = cv2.VideoCapture('snow.avi')
+cap = cv2.VideoCapture('highway.avi')
 ret, img = cap.read()
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -75,17 +75,17 @@ rows, cols = img.shape
 
 MAX_hamming_weight = rows*cols*N
 
-background_pictures = np.ones((rows,cols,N))*255;
-update_background_pictures(img, background_pictures, rows, cols)
+gradient_pictures = np.ones((rows,cols,N))*255;
+update_gradient_pictures(img, gradient_pictures, rows, cols)
 
 while True:
     ret, img = cap.read()
     if ret == False:
         break
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    update_background_pictures(img, background_pictures, rows, cols)
-    foreback = lbsp(img, background_pictures)
-    cv2.imshow('original',foreback)
+    update_gradient_pictures(img, gradient_pictures, rows, cols)
+    gradient = lbsp(img, gradient_pictures)
+    cv2.imshow('original',gradient)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
