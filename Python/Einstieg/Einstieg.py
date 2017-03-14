1adf0abcb1be7c46b4b56a2e6f12303ee4245aaa
Jonas Bühlmeyer <jonas.buehlmeyer@robotics-erlangen.de>
2017-03-14 18:21:58 +0100

    Einstieg

--- Python/Einstieg/Einstieg.py
+++ Python/Einstieg/Einstieg.py
@@ -1,21 +1,20 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('bild.png')
-rows, cols, channels = img.shape
+blured = cv2.imread('blured.jpg')
+original = cv2.imread('original.jpg')
+rows, cols, channels = blured.shape
 
-img1 = img[:,15:cols/2,:]
-img2 = img[:,cols/2+15:cols,:]
-
-img3 = img[2:rows,15:cols/2-17,:]
-img4 = img[:rows-2,cols/2+32:cols,:]
 
 
 while True:
-    cv2.imshow('3',img3)
-    cv2.imshow('4',img4)
-    cv2.imshow('1+2', img1/2+img2/2)
-    cv2.imshow('3+4', img3/2+img4/2)
+    cv2.imshow('blured',blured)
+    cv2.imshow('original',original[20:rows-50,0:cols-50,:])
+    cv2.imshow('+', original/2+blured/2)
+    cv2.imshow('test',(original/2+blured/2)[20:rows-50,0:cols-50,:])
     
+    cv2.imwrite('original_small',original[20:rows-50,0:cols-50,:])
+    cv2.imwrite('overlay_small',(original/2+blured/2)[20:rows-50,0:cols-50,:])
+
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
